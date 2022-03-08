82a06dfef59d911f7e81bb96c77be705e4a65782
Ryan Kuruppu <ryanjk.kuruppu@gmail.com>
2022-03-08 19:08:55 +0530

    Added 3 Controllers for the 3 required views and updated their Tab Bar icons

---
+++ FinCal/Controllers/SavingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SavingsViewController.swift
+//  FinCal
+//
+//  Created by Ryan Kuruppu on 8/3/22.
+//
+
+import UIKit
+
+class SavingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = ScreenNames.savingScreen
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FinCal/Models/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  FinCal
+//
+//  Created by Ryan Kuruppu on 6/3/22.
+//
+
+import Foundation
+
+enum Color {
+    
+}
+
+enum ScreenNames {
+    static let savingScreen = "Savings"
+    static let loanMortgageScreen = "Loan & Mortgage"
+    static let compoundInterestScreen = "Compound Savings"
+}
