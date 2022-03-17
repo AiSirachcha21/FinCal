270fa214696411c703f636c9ee0305c24db7f2f0
Ryan Kuruppu <ryanjk.kuruppu@gmail.com>
2022-03-17 20:21:12 +0530

    Added RxSwift and updated SavingsViewController to build subscriptions based on one text field for testing

--- FinCal/Controllers/SavingsViewController.swift
+++ FinCal/Controllers/SavingsViewController.swift
@@ -6,76 +6,63 @@
 //
 
 import UIKit
-import Combine
+import RxSwift
+import RxCocoa
+
+struct SavingFormFieldObservables {
+    var principalAmount: Observable<Optional<String>>
+}
 
 class SavingsViewController: UIViewController {
-    @IBOutlet var simpleSavingsFormFields: [UITextField]!
-
-    @Published var simpleSavings = SimpleSavings(principalAmount: 0.0)
-    var cancellable: AnyCancellable?
-
+    
+    @IBOutlet var principalAmountTF: UITextField!
+    
+    private var disposeBag = DisposeBag()
+    private var simpleSavings = SimpleSavings()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
         self.hideKeyboardWhenSwipeDown()
-
+        
         title = "Savings"
-
+        
         // TODO: Action needs to be implemented here for the "Help View"
         let questionImage = UIImage(systemName: "questionmark.circle", withConfiguration: UIImage.SymbolConfiguration(scale: .default))
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: questionImage, style: .plain, target: self, action: nil)
-
-        simpleSavingsFormFields.forEach { $0.addNumericAccessory(addPlusMinus: true) }
+        
+        //        simpleSavingsFormFields.forEach { $0.addNumericAccessory(addPlusMinus: true) }q
+        let observables = setupInputBindings()
+        setupSubscriptions(observables)
+        
     }
-
-    // TODO: Fields auto detect when changing principal value or interest rate. Must test with other fields
-    // TODO: Make sure reverse works (predicting principal value or interest when other fields are entered)
-    // TODO: Optimize code by finding a better way to implement Combine and moving commonly used code to classes/structs
-    @IBAction func onEdited(_ sender: UITextField) {
-        if let senderText = sender.text {
-            switch sender.tag {
-            case TextFieldID.principalAmount.rawValue:
-                self.simpleSavings.principalAmount = Double(senderText) ?? 0.0
-            case TextFieldID.interest.rawValue:
-                self.simpleSavings.interest = (Double(senderText) ?? 0.0) / 100
-            case TextFieldID.numberOfPayments.rawValue:
-                self.simpleSavings.numberOfPayments = Int(senderText) ?? 0
-            case TextFieldID.duration.rawValue:
-                self.simpleSavings.duration = Int(senderText) ?? 0
-            case TextFieldID.futureValue.rawValue:
-                self.simpleSavings.futureValue = Double(senderText) ?? 0.0
-            default:
-                return
-            }
-        }
-
-        self.cancellable = self.$simpleSavings
-            .debounce(for: .milliseconds(1500), scheduler: DispatchQueue.main, options: .none)
-            .receive(on: DispatchQueue.main)
-            .sink(receiveValue: { [weak self] value in
-            guard let formFields = self?.simpleSavingsFormFields else { return }
-
-            formFields.first { $0.tag == TextFieldID.futureValue.rawValue }?.text = value
-                .getFutureValue(updateOriginal: true)
-                .roundTo(decimalPlaces: 2)
-                .description
-
-            formFields.first { $0.tag == TextFieldID.numberOfPayments.rawValue }?.text = value
-                .numberOfPayments
-                .description
-
-            formFields.first { $0.tag == TextFieldID.duration.rawValue }?.text = value
-                .duration
-                .description
-
-            formFields.first { $0.tag == TextFieldID.interest.rawValue }?.text = value
-                .getInterestRateAsPercentage()
-                .roundTo(decimalPlaces: 2)
-                .description
-        })
+    
+    func setupInputBindings() -> SavingFormFieldObservables {
+        let principleAmount = principalAmountTF
+            .rx
+            .text
+            .observe(on: MainScheduler.asyncInstance)
+            .distinctUntilChanged()
+        
+        return SavingFormFieldObservables(principalAmount: principleAmount)
     }
-
-    override func viewDidDisappear(_ animated: Bool) {
-        cancellable?.cancel()
+    
+    func setupSubscriptions(_ observables: SavingFormFieldObservables) {
+        observables.principalAmount
+            .debounce(.milliseconds(1500), scheduler: MainScheduler.asyncInstance)
+            .subscribe(
+                onNext: { [weak self] value in
+                    guard let text = value else {
+                        self?.simpleSavings.principalAmount = 0.0
+                        return
+                    }
+                    self?.simpleSavings.principalAmount = Double(text) ?? 0.0
+                    
+                    print(self?.simpleSavings.principalAmount ?? "Nothing here")
+                },
+                onError: { _ in },
+                onCompleted: nil,
+                onDisposed: nil )
+            .disposed(by: disposeBag)
     }
 }
--- FinCal/Models/CompoundSavings.swift
+++ FinCal/Models/CompoundSavings.swift
@@ -7,20 +7,19 @@
 
 import Foundation
 
-class CompoundSavings: Savings, MonthlyPayable {
+class CompoundSavings {
     var monthlyPayment: Double
     var futureValue: Double
     var principalAmount: Double
     var interest: Double
     var numberOfPayments: Int
-    
-    init(monthlyPayment:Double, futureValue:Double, principalAmount:Double, interest:Double, numberOfPayments:Int) {
+
+    init(monthlyPayment: Double, futureValue: Double, principalAmount: Double, interest: Double, numberOfPayments: Int) {
         self.futureValue = futureValue
         self.monthlyPayment = monthlyPayment
         self.principalAmount = principalAmount
         self.interest = interest
         self.numberOfPayments = numberOfPayments
     }
-    
-    
+
 }
